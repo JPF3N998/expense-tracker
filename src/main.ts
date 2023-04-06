ce16b0dcbe19d58dcf37bc7bc3ed1c6e8da1f0e3
JP <jpfeng-dev@proton.me>
2023-04-06 12:51:52 -0600

    Registered FormKit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
+import { plugin, defaultConfig } from '@formkit/vue'
 import '@/style.css'
 import '@config/injectFluentDesignSystem'
 
 import App from '@/App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(plugin, defaultConfig)
+  .mount('#app')
