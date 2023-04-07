cbf2d81ffe4ea2a8c7ceee5c339c4592cb395f54
JP <jpfeng-dev@proton.me>
2023-04-07 13:32:24 -0600

    Added unit tests for TheForm

--- tests/components/TheForm.test.ts
+++ tests/components/TheForm.test.ts
@@ -1,15 +1,41 @@
-import TheForm from '@components/TheForm.vue';
-import { render, screen } from '@testing-library/vue';
+import { render, RenderResult, screen } from '@testing-library/vue';
 import { beforeEach, describe, expect, it } from 'vitest';
+import { plugin, defaultConfig } from '@formkit/vue';
+
+import TheForm from '@components/TheForm.vue';
+
+// Registering global plugins (FormKit, for instance) in test
+// LINK https://stackblitz.com/edit/github-k2bhcr?file=components%2FFormExample.test.js
 
 describe('TheForm', () => {
   describe('Rendering', () => {
+    let dom: RenderResult;
+
     beforeEach(() => {
-      render(TheForm);
+      dom = render(TheForm, {
+        global: {
+          plugins: [[plugin, defaultConfig()]],
+        },
+      });
     });
 
-    it('should render the correct form fields', () => {
-      console.log(screen);
+    describe('the correct fields', () => {
+      const targetFieldNames = [
+        'name',
+        'amount',
+        'details',
+        'emoji',
+        'currency',
+      ];
+
+      let el: HTMLElement;
+
+      targetFieldNames.forEach((name) => {
+        it(`should render field with name: "${name}"`, async () => {
+          el = document.querySelector(`[name="${name}"]`);
+          expect(el).toBeTruthy();
+        });
+      });
     });
   });
 });
