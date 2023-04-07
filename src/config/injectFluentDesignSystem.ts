7d372bb71fa39082d7afc3897a65a7150031d752
JP <jpfeng-dev@proton.me>
2023-04-07 13:30:00 -0600

    Replaced FormKit vanilla select with Fluent UI one

--- src/config/injectFluentDesignSystem.ts
+++ src/config/injectFluentDesignSystem.ts
@@ -1,18 +1,22 @@
 import {
-  provideFluentDesignSystem,
-  fluentTextField,
   fluentNumberField,
-  fluentTextArea
+  fluentOption,
+  fluentSelect,
+  fluentTextArea,
+  fluentTextField,
+  provideFluentDesignSystem,
 } from '@fluentui/web-components';
 
 const fluentDesignSystem = provideFluentDesignSystem();
 
 export const components = [
   fluentNumberField,
+  fluentOption,
+  fluentSelect,
   fluentTextArea,
   fluentTextField,
-]
+];
 
 components.forEach((component) => {
   fluentDesignSystem.register(component());
-})
+});
