177b5377a42023f30f26b31cdde01c77f74b40e0
JP <jpfeng-dev@proton.me>
2023-04-06 23:09:35 -0600

    Updates to TheForm component
    + Added initial values to schema
    + Printed submitted/generated object

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,89 +1,96 @@
 <script lang="ts" setup>
 import { ref, Ref } from 'vue';
 import CONSTANTS from '@constants'
-import Record from '@models/Record';
+import Transaction from '@models/Transaction';
 
 const { CURRENCIES, FUIC } = CONSTANTS
 
-const data: Ref<Record> = ref({
+const data: Ref<Transaction> = ref({
   name: '',
   amount: 0,
-  description: '',
-  emoji: undefined,
+  details: '',
+  emoji: '',
   currency: 'USD',
 })
 
-// https://github.com/formkit/formkit/issues/643
+// LINK: [How to modify sectionsSchema as prop](https://github.com/formkit/formkit/issues/643)
 const schema = [
   {
     $el: 'h1',
-    children: 'Add new record'
+    children: 'Add new transaction'
   },
   {
-    $formkit: 'text',
-    name: 'name',
-    sectionsSchema: {
-      input: { $el: FUIC.fluentTextField }
-    },
-    label: 'Record name',
-    help: 'Record identifier name',
-    validation: 'required|email'
+    $formkit: 'group',
+    name: 'transaction',
+    children: [
+      {
+        $formkit: 'text',
+        name: 'name',
+        sectionsSchema: {
+          input: { $el: FUIC.fluentTextField }
+        },
+        label: 'Transaction name',
+        placeholder: '"New phone"',
+        validation: 'required',
+      },
+      {
+        $formkit: 'number',
+        min: 0,
+        step: 0.1,
+        required: true,
+        name: 'amount',
+        label: 'Amount',
+        sectionsSchema: {
+          input: { $el: FUIC.fluentNumberField }
+        },
+        value: '0'
+      },
+      {
+        $formkit: 'textarea',
+        label: 'Details',
+        name: 'details',
+        cols: 20,
+        maxLength: 200,
+        placeholder: 'Additional information...',
+        sectionsSchema: {
+          input: { $el: FUIC.fluentTextArea }
+        },
+        value: ''
+      },
+      {
+        $formkit: 'text',
+        label: 'Emoji',
+        name: 'emoji',
+        maxLength: 2,
+        sectionsSchema: {
+          input: { $el: FUIC.fluentTextField }
+        },
+      // Validation: check if only emoji, show emoji chooser in the future
+      },
+      {
+        $formkit: 'select',
+        name: 'currency',
+        label: 'Currency',
+        options: CURRENCIES,
+      }
+    ]
   },
-  {
-    $formkit: 'number',
-    min: 0,
-    step: 0.1,
-    required: true,
-    name: 'amount',
-    label: 'Amount',
-    sectionsSchema: {
-      input: { $el: FUIC.fluentNumberField }
-    }
-  },
-  {
-    $formkit: 'textarea',
-    label: 'Description',
-    name: 'description',
-    cols: 20,
-    maxLength: 200,
-    validation: 'required',
-    sectionsSchema: {
-      input: { $el: FUIC.fluentTextArea }
-    }
-  },
-  {
-    $formkit: 'text',
-    label: 'Emoji',
-    name: 'emoji',
-    maxLength: 2,
-    sectionsSchema: {
-      input: { $el: FUIC.fluentTextField }
-    },
-    // Validation: check if only emoji, show emoji chooser in the future
-  },
-  {
-    $formkit: 'select',
-    name: 'currency',
-    label: 'Currency',
-    options: CURRENCIES,
-    value: 'USD'
-  }
 ]
 
-function handleSubmit(e: Event) {
-  console.log('fired');
-  
-  const { value } = e.target as HTMLInputElement
-  data.value.name = value
+const tx: Ref<Transaction | null> = ref(null)
+function handleSubmit(submittedData: Transaction) {
+  tx.value = submittedData
 }
 
 </script>
 
 <template>
   <div>
-    <FormKit type="form" v-model="data">
+    <FormKit type="form" @submit="handleSubmit">
       <FormKitSchema :schema="schema" />
     </FormKit>
-    {{ data }}
+    <p>
+      {{ tx }}
+    </p>
   </div>
 </template>
