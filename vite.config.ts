e54fd7777664ab142bb70e48d64d527b58886c42
JP <jpfeng-dev@proton.me>
2023-04-07 01:02:20 -0600

    Updated mechanism to load path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,41 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
+// @ts-ignore
+import pathAliases from './pathAliases.json';
 
 const getResolved = (path: string) => {
-  return resolve(__dirname, path)
-}
+  return resolve(__dirname, path);
+};
 
-const alias = {
-  '@': getResolved('src'),
-  '@components': getResolved('src/components'),
-  '@config': getResolved('src/config'),
-  '@constants': getResolved('src/constants'),
-  '@models': getResolved('src/models'),
-  '@views': getResolved('src/views'),
-}
+/**
+ * Reads
+ * @returns Object with the path aliases
+ */
+const getPathAliases = (aliasesObj: Object) => {
+  const output = Object.entries(aliasesObj).reduce(
+    (accum, [aliasStr, path]: [string, string]) => {
+      accum[aliasStr] = getResolved(path);
+      return accum;
+    },
+    {}
+  );
+  console.info(`Aliases made: ${Object.keys(output)}`);
+
+  return output;
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    template: {
-      compilerOptions: {
-        isCustomElement: (tag) => tag.includes('fluent-')
-      }
-    }
-  })],
-  resolve: { alias }
-})
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.includes('fluent-'),
+        },
+      },
+    }),
+  ],
+  resolve: { alias: getPathAliases(pathAliases) },
+});
