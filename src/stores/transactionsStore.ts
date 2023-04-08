5170759306c6eb388073423ab768fdd51890aa90
JP <jpfeng-dev@proton.me>
2023-04-07 21:59:27 -0600

    Added transaction addition action on store

--- src/stores/transactionsStore.ts
+++ src/stores/transactionsStore.ts
@@ -5,7 +5,15 @@ import { ref, Ref } from 'vue';
 export const useTransactionsStore = defineStore('transactions', () => {
   const transactions: Ref<Transaction[]> = ref([]);
 
+  function commitNewTransaction(newTransaction: Transaction) {
+    transactions.value.push(newTransaction);
+  }
+
   return {
+    // State
     transactions,
+
+    // Actions
+    commitNewTransaction,
   };
 });
