c4b59341a588cec753007cf026db925ef804df2d
JP <jpfeng-dev@proton.me>
2023-04-07 21:51:17 -0600

    Added Pinia to project

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { createApp } from 'vue'
-import { plugin, defaultConfig } from '@formkit/vue'
-import '@/style.css'
-import '@config/injectFluentDesignSystem'
+import { createApp } from 'vue';
+import { plugin, defaultConfig } from '@formkit/vue';
+import { createPinia } from 'pinia';
+import '@/style.css';
+import '@config/injectFluentDesignSystem';
 
-import App from '@/App.vue'
+import App from '@/App.vue';
 
-createApp(App)
-  .use(plugin, defaultConfig)
-  .mount('#app')
+const pinia = createPinia();
+
+createApp(App).use(plugin, defaultConfig).use(pinia).mount('#app');
