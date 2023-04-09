2ebd12d67f30f9efffb3bf09442745da6d05850d
JP <jpfeng-dev@proton.me>
2023-04-08 23:21:56 -0600

    Created TheFormWrapper component

---
+++ src/components/modal_bodies/TheFormWrapper.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TheForm from '@components/TheForm.vue';
+</script>
+
+<template>
+  <div class="formWrapper">
+    <TheForm />
+  </div>
+</template>
+
+<style scoped>
+.formWrapper {
+  padding: 1rem;
+}
+</style>
