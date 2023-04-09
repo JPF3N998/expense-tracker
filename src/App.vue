f01c36f088542c4bfce1a55214ee6d52f6ddc8be
JP <jpfeng-dev@proton.me>
2023-04-08 19:34:49 -0600

    Added OpenDialogButton to App

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,27 @@
 import Layout from '@views/Layout.vue'
 import TheForm from '@components/TheForm.vue';
 import TransactionsAccordion from '@components/TransactionsAccordion.vue';
+import OpenDialogButton from '@components/OpenDialogButton.vue';
 </script>
 
 <template>
   <Layout>
     <TheForm />
-    <TransactionsAccordion #sidenav />
+    <TransactionsAccordion />
+
+    <div class="ctaWrapper">
+      <OpenDialogButton :button-text="'+ Add new transaction'" v-slot="slotProps">
+        <h1>Dialog body</h1>
+        <button @click="slotProps.closeDialog">Close</button>
+      </OpenDialogButton>
+    </div>
   </Layout>
 </template>
+
+<style scoped>
+.ctaWrapper {
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+}
+</style>
