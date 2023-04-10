adf4f36ddf2152e9fb1f0fa62ba0cf3ce69242d4
JP <jpfeng-dev@proton.me>
2023-04-10 12:58:17 -0600

    Learned how to tap into FAST design coloring system

--- src/config/injectFluentDesignSystem.ts
+++ src/config/injectFluentDesignSystem.ts
@@ -1,4 +1,5 @@
 import {
+  accentPalette,
   fluentAccordion,
   fluentAccordionItem,
   fluentButton,
@@ -7,7 +8,9 @@ import {
   fluentSelect,
   fluentTextArea,
   fluentTextField,
+  PaletteRGB,
   provideFluentDesignSystem,
+  SwatchRGB,
 } from '@fluentui/web-components';
 
 const fluentDesignSystem = provideFluentDesignSystem();
@@ -22,6 +25,14 @@ export const components = [
   fluentTextArea,
   fluentTextField,
 ];
+
+// LINK: Fluent Color Palette change:
+// 1. https://github.com/microsoft/fast/blob/master/examples/design-system-tutorial/src/main.ts#L123
+// 2. https://github.com/microsoft/fluentui/blob/677a67b551966986db34a6fac608cb89ee150471/packages/web-components/src/design-tokens.ts#L315
+
+accentPalette.withDefault(
+  PaletteRGB.from(SwatchRGB.from({ r: 148, g: 112, b: 244 }))
+);
 
 components.forEach((component) => {
   fluentDesignSystem.register(component());
